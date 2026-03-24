0dde2229ce795df0e4b5ad1cad0fb573cb1bbf4d
Allu Nagasai Yashwanth Reddy <allusaireddy123@gmail.com>
2026-03-25 00:45:47 +0530

    Update ArrayProblems.java

--- ArrayProblems.java
+++ ArrayProblems.java
@@ -65,34 +65,74 @@ public class ArrayProblems {
 		System.out.println("Highest: "+high+", lowest :"+low);
 		
 		
+		//  copy data from one array to another array
+		
+		String[] names1 = {"Java","Python","C","C++"};
+		
+		String[] names2 = new String[names1.length];
+		
+		for(int i=0;i<names1.length;i++) {
+			names2[i] = names1[i];
+		}
 		
 		
+		// Swapping 
 		
+		int a = 10;
+		int b = 20;
 		
+		int temp = a; // 10
+		a = b; // 20 
+		b = temp; // 10
 		
+
+				//	  i 			 j	
+				//	  0, 1, 2, 3, 4, 5
+		int[] arr = {10,20,30,40,50,60};
 		
+		int i = 0;
+		int j = arr.length-1;
 		
+		while(i<j) {
 		
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--; // j-1 arr.length-1-1  // arr.length-2	
+	
+		}
 		
+		for(int data : arr) {
+			System.out.print(data+" ");
+		}
 		
+		System.out.println("");
 		
+		// Second Highest
 		
+		int[] array = {30,25,19,9,1,60};
 		
+		int Highest = arr[0];	//30
+		int secondHighest = arr[0];	//30
 		
+		for(int i1=1;i1<array.length;i1++) {
+			
+			int ele = arr[i]; 
+			
+			if(ele != Highest) {
+				
+				if(ele > Highest) {
+					secondHighest = Highest;
+					Highest = ele;
+				}else if(ele > secondHighest || Highest == secondHighest) {
+					secondHighest = ele;
+				}
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println("Highest : "+Highest);
+		System.out.println("SecondHighest : "+secondHighest);
 		
 	}
 
